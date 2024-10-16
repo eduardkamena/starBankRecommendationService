b7d026be1694443dcf1a1b71917eb5c5749bf1fc
Ed <eduard@kamenskikh.me>
2024-10-17 02:27:04 +0300

    Добавил конфигурацию DataSource и JdbcTemplate

---
+++ src/main/java/pro/sky/recommendation_service/configuration/RecommendationsDataSourceConfiguration.java
@@ -0,0 +1,29 @@
+package pro.sky.recommendation_service.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+public class RecommendationsDataSourceConfiguration {
+
+    @Bean(name = "recommendationsDataSource")
+    public DataSource recommendationsDataSource(@Value("${application.recommendations-db.url}") String recommendationsUrl) {
+        var dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(recommendationsUrl);
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setReadOnly(true);
+        return dataSource;
+    }
+
+    @Bean(name = "recommendationsJdbcTemplate")
+    public JdbcTemplate recommendationsJdbcTemplate(
+            @Qualifier("recommendationsDataSource") DataSource dataSource
+    ) {
+        return new JdbcTemplate(dataSource);
+    }
+
+}
