a25eb066611103e54d5bb5589334ccda87ad1d66
gitWestender <javaxwestender@gmail.com>
2024-10-19 21:39:07 +0300

    refactor add API annotation

--- src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
+++ src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
@@ -1,5 +1,6 @@
 package pro.sky.recommendation_service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,10 +8,14 @@ import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Сущность рекомендации")
 public class RecommendationDTO {
 
+    @Schema(description = "Назвиние рекомендации")
     private final String name;
+    @Schema(description = "Идентификатор рекомендации")
     private final UUID id;
+    @Schema(description = "Описание рекомендации")
     private final String text;
 
 }
