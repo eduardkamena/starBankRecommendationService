ea6dcd1056cd03d45f2c2f843c7e688f310e4581
gitWestender <javaxwestender@gmail.com>
2024-10-20 21:23:09 +0300

    refactor delete unused annotations

--- src/main/java/pro/sky/recommendation_service/exception/AppError.java
+++ src/main/java/pro/sky/recommendation_service/exception/AppError.java
@@ -1,18 +1,14 @@
 package pro.sky.recommendation_service.exception;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-@Schema(description = "Сущность ошибки")
 public class AppError {
 
-    @Schema(description = "Статус ошибки")
     private final int statusCode;
 
-    @Schema(description = "Сообщение ошибки")
     private final String message;
 
 }
