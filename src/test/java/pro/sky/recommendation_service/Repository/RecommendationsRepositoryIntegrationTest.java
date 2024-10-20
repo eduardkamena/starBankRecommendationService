74a506e9fdf7fb78d5b0f533f89d3ad95a2c7b3b
aleksandrgovorin <govorinsasha0@gmail.com>
2024-10-20 21:05:03 +0200

    Test commit

---
+++ src/test/java/pro/sky/recommendation_service/Repository/RecommendationsRepositoryIntegrationTest.java
@@ -0,0 +1,51 @@
+package pro.sky.recommendation_service.Repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import pro.sky.recommendation_service.repository.RecommendationsRepository;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Sql({"/test-data.sql"})
+class RecommendationsRepositoryIntegrationTest {
+
+    @Autowired
+    private RecommendationsRepository recommendationsRepository;
+
+    @Test
+    void testGetTransactionAmount() {
+        UUID userId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+
+        int amount = recommendationsRepository.getTransactionAmount(userId, "INVESTMENT", "DEPOSIT");
+        assertEquals(5000, amount);
+    }
+
+    @Test
+    void testIsProductExists() {
+        UUID userId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+        boolean exists = recommendationsRepository.isProductExists(userId, "LOAN");
+
+        assertTrue(exists);
+    }
+    @Test
+    void testIsUserExists() {
+        UUID userId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+
+        boolean userExists = recommendationsRepository.isUserExists(userId);
+
+        assertTrue(userExists);
+    }
+
+    @Test
+    void testIsUserNotExists() {
+        UUID userId = UUID.randomUUID();
+
+        boolean userExists = recommendationsRepository.isUserExists(userId);
+
+        assertFalse(userExists);
+    }
+}
