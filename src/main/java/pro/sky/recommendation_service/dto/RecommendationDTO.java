ea4cca9489dd1310cd55af59a859b258caa4c4a6
gitWestender <javaxwestender@gmail.com>
2024-10-21 22:11:25 +0300

    refactor add List roles

--- src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
+++ src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
@@ -3,7 +3,9 @@ package pro.sky.recommendation_service.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import pro.sky.recommendation_service.enitites.Rule;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -19,5 +21,8 @@ public class RecommendationDTO {
 
     @Schema(description = "Описание рекомендации")
     private final String text;
+
+    @Schema(description = "Список правил")
+    private final List<Rule> rules;
 
 }
