ddd638785a429bda9667b46abdb8b645d8fa3353
gitWestender <javaxwestender@gmail.com>
2024-10-21 23:44:10 +0300

    refactor some annotations (WIP)

--- src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
+++ src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
@@ -1,7 +1,6 @@
 package pro.sky.recommendation_service.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import pro.sky.recommendation_service.enitites.Rule;
