9a485769d972418dc12d7c70b837f2e044d7a9dc
gitWestender <javaxwestender@gmail.com>
2024-10-21 23:43:50 +0300

    refactor some annotations (WIP)

--- src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
+++ src/main/java/pro/sky/recommendation_service/dto/RecommendationDTO.java
@@ -3,13 +3,14 @@ package pro.sky.recommendation_service.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import pro.sky.recommendation_service.enitites.Rule;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Schema(description = "Сущность рекомендации")
 public class RecommendationDTO {
 
@@ -23,6 +24,6 @@ public class RecommendationDTO {
     private final String text;
 
     @Schema(description = "Список правил")
-    private final List<Rule> rules;
+    private List<Rule> rules;
 
 }
