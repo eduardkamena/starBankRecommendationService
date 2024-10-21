d4f5f13d209f5e8fd7c03aec296b8e03a5c4212d
gitWestender <javaxwestender@gmail.com>
2024-10-21 20:05:15 +0300

    feature create Rule entity

---
+++ src/main/java/pro/sky/recommendation_service/enitites/Rule.java
@@ -0,0 +1,13 @@
+package pro.sky.recommendation_service.enitites;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Rule {
+    private String query;
+    private List<String> arguments;
+    private boolean negate;
+
+}
