871d12d2cc2216fb295685d0b79831c7f04c129e
gitWestender <javaxwestender@gmail.com>
2024-10-22 21:29:51 +0300

    refactor DataSourceConfiguration class

--- src/main/java/pro/sky/recommendation_service/configuration/RecommendationRulesDataSourceConfiguration.java
+++ src/main/java/pro/sky/recommendation_service/configuration/RecommendationRulesDataSourceConfiguration.java
@@ -1,28 +1,45 @@
 package pro.sky.recommendation_service.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class RecommendationRulesDataSourceConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(RecommendationRulesDataSourceConfiguration.class);
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.secondary")
+    public DataSourceProperties secondaryDataSourceProperties() {
+        DataSourceProperties properties = new DataSourceProperties();
+        logger.info("Second(Rules) DataSource Properties: {}", properties);
+        return properties;
+    }
+
     @Bean(name = "rulesDataSource")
-    public DataSource rulesDataSource(
-            @Value("${application.recommendation-rules-db.url}") String rulesUrl) {
-        var dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(rulesUrl);
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setReadOnly(false);
+    public DataSource secondaryDataSource() {
+        DataSource dataSource = secondaryDataSourceProperties().initializeDataSourceBuilder().build();
+        logger.info("Second(Rules) DataSource: {}", dataSource);
         return dataSource;
     }
 
-    @Bean(name = "rulesJdbcTemplate")
+    @Bean
+    public PlatformTransactionManager secondaryTransactionalManager(
+            @Qualifier("rulesDataSource") DataSource secondaryDataSource) {
+        return new DataSourceTransactionManager(secondaryDataSource);
+    }
+
+   @Bean(name = "rulesJdbcTemplate")
     public JdbcTemplate rulesJdbcTemplate(
             @Qualifier("rulesDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
--- src/main/java/pro/sky/recommendation_service/configuration/RecommendationsDataSourceConfiguration.java
+++ src/main/java/pro/sky/recommendation_service/configuration/RecommendationsDataSourceConfiguration.java
@@ -1,34 +1,52 @@
 package pro.sky.recommendation_service.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class RecommendationsDataSourceConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(RecommendationsDataSourceConfiguration.class);
     // Добавление источника данных — DataSource
     @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.primary")
+    public DataSourceProperties primaryDataSourceProperties() {
+        DataSourceProperties properties = new DataSourceProperties();
+        logger.info("First(Transactions) DataSource Properties: {}", properties);
+        return properties;
+    }
+
+    @Primary
     @Bean(name = "recommendationsDataSource")
-    @Value("${application.recommendations-db.url}")
-    public DataSource recommendationsDataSource(String recommendationsUrl) {
-        var dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(recommendationsUrl);
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setReadOnly(true);
+    public DataSource recommendationsDataSource(){
+        DataSource dataSource = primaryDataSourceProperties().initializeDataSourceBuilder().build();
+        logger.info("First(Transactions) DataSource: {}", dataSource);
         return dataSource;
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager firstTransactionalManager(
+            @Qualifier("recommendationsDataSource") DataSource primaryDataSource) {
+        return new DataSourceTransactionManager(primaryDataSource);
     }
 
     // Добавление конфигурации для JdbcTemplate
     @Bean(name = "recommendationsJdbcTemplate")
-    @Qualifier("recommendationsDataSource")
-    public JdbcTemplate recommendationsJdbcTemplate(DataSource dataSource) {
+    public JdbcTemplate recommendationsJdbcTemplate(
+            @Qualifier("recommendationsDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
 
