9171a3644ea7db18965dd86fbb8bd72cb868ab00
gitWestender <javaxwestender@gmail.com>
2024-10-23 09:41:52 +0300

    refactor add liquibase config

--- src/main/java/pro/sky/recommendation_service/configuration/RecommendationRulesDataSourceConfiguration.java
+++ src/main/java/pro/sky/recommendation_service/configuration/RecommendationRulesDataSourceConfiguration.java
@@ -1,9 +1,11 @@
 package pro.sky.recommendation_service.configuration;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,5 +45,30 @@ public class RecommendationRulesDataSourceConfiguration {
     public JdbcTemplate rulesJdbcTemplate(
             @Qualifier("rulesDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.secondary.liquibase")
+    public LiquibaseProperties secondaryLiquibaseProperties() {
+        return new LiquibaseProperties();
+    }
+
+    @Bean
+    public SpringLiquibase secondaryLiquibase(){
+        return secondaryLiquibase(secondaryDataSource(), secondaryLiquibaseProperties());
+    }
+
+    public static SpringLiquibase secondaryLiquibase(
+            DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setShouldRun(liquibaseProperties.isEnabled());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setRollbackFile(liquibaseProperties.getRollbackFile());
+        return liquibase;
     }
 }
