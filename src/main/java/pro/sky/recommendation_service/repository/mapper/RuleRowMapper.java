bf0aa7443bcd57caaf4dcd7d3bfedd5ec027e136
gitWestender <javaxwestender@gmail.com>
2024-10-24 10:12:56 +0300

    replace

---
+++ src/main/java/pro/sky/recommendation_service/repository/mapper/RuleRowMapper.java
@@ -0,0 +1,19 @@
+package pro.sky.recommendation_service.repository.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import pro.sky.recommendation_service.entity.Rule;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RuleRowMapper implements RowMapper<Rule> {
+    @Override
+    public Rule mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Rule rule = new Rule();
+        rule.setQuery(rs.getString("query"));
+        rule.setArguments(rs.getObject("arguments", String.class));
+        rule.setNegate(rs.getBoolean("negate"));
+
+        return rule;
+    }
+}
