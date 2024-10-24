ae4913c40292985a9cdc4e2912f0212704cd7090
gitWestender <javaxwestender@gmail.com>
2024-10-24 10:13:36 +0300

    refactor add identification

--- src/main/java/pro/sky/recommendation_service/entity/Rule.java
+++ src/main/java/pro/sky/recommendation_service/entity/Rule.java
@@ -2,9 +2,12 @@ package pro.sky.recommendation_service.entity;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class Rule {
 
+    private UUID id;
     private String query;
     private String arguments;
     private boolean negate;
