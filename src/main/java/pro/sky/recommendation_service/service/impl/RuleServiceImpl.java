51cfc9e85fbeab9f49009512c7b07108168b52db
gitWestender <javaxwestender@gmail.com>
2024-10-25 19:28:48 +0300

    refactor implementation methods feature checkArguments method

--- src/main/java/pro/sky/recommendation_service/service/impl/RuleServiceImpl.java
+++ src/main/java/pro/sky/recommendation_service/service/impl/RuleServiceImpl.java
@@ -22,11 +22,63 @@ public class RuleServiceImpl implements RulesService {
 
     @Override
     public Rule createRule(Rule rule) {
-        return null;
+        try {
+            checkArguments(rule);
+            rulesRepository.addRule(rule);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return rule;
     }
 
     @Override
-    public void deleteRule(UUID ruleId) {
-        rulesRepository.deleteRuleById(ruleId.toString());
+    public void deleteRule(UUID id) {
+        rulesRepository.deleteRuleById(id);
     }
+
+    /**
+     * Метод проверки, позволяющий узнать является ли строка положительным числом в пределах Integer
+     *
+     * @param str строка для проверки
+     * @return true - если число положительное И в пределах Integer
+     *         false - если число отрицательное ИЛИ за пределами Integer
+     */
+    public boolean isPositiveNumber(String str) {
+        try {
+            return Integer.parseInt(str) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Проверяет передаваемые значения аргументов
+     *
+     * @param rule правило, чьи аргументы будут проверяться на соответствие
+     * @throws IllegalArgumentException если аргументы не соответствуют или отсутствуют вовсе
+     */
+    public void checkArguments(Rule rule) {
+        String[] arguments = rule.getArguments();
+        if (rule.getQuery().isEmpty() || rule.getArguments().length == 0) {
+            throw new IllegalArgumentException("Rule must have at least one argument");
+        }
+
+        for (String argument : arguments) {
+            if (isPositiveNumber(argument)
+            ||argument.equals("DEBIT")
+            || argument.equals("CREDIT")
+            || argument.equals("INVEST")
+            || argument.equals("DEPOSIT")
+            || argument.equals(">")
+            || argument.equals(">=")
+            || argument.equals("<")
+            || argument.equals("<=")
+            || argument.equals("=")) {
+            } else {
+                throw new IllegalArgumentException("Rule must have at least one argument");
+            }
+        }
+    }
+
+
 }
