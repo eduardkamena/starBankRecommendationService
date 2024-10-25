2daef120df34569ecaae873c613b2185082b9b89
gitWestender <javaxwestender@gmail.com>
2024-10-25 20:56:11 +0300

    refactor

--- src/main/java/pro/sky/recommendation_service/service/RulesService.java
+++ src/main/java/pro/sky/recommendation_service/service/RulesService.java
@@ -2,13 +2,12 @@ package pro.sky.recommendation_service.service;
 
 import pro.sky.recommendation_service.entity.Rule;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
 
 public interface RulesService {
 
-    List<Rule> getAllRules() throws SQLException;
+    List<Rule> getAllRules();
     Rule createRule(Rule rule);
     void deleteRule(UUID id);
 
