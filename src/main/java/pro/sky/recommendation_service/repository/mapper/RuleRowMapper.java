d3603b7e9a0c643133116eb185dc91428c560c16
gitWestender <javaxwestender@gmail.com>
2024-10-28 00:00:32 +0300

    complete rowMapper

--- src/main/java/pro/sky/recommendation_service/repository/mapper/RuleRowMapper.java
+++ src/main/java/pro/sky/recommendation_service/repository/mapper/RuleRowMapper.java
@@ -7,9 +7,7 @@ import pro.sky.recommendation_service.entity.enums.Queries;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class RuleRowMapper implements RowMapper<RecommendationRule> {
     private final JdbcTemplate jdbcTemplate;
@@ -17,22 +15,45 @@ public class RuleRowMapper implements RowMapper<RecommendationRule> {
     public RuleRowMapper(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    private final Map<UUID, RecommendationRule> ruleMap = new HashMap<>();
+
     @Override
     public RecommendationRule mapRow(ResultSet rs, int rowNum) throws SQLException {
-
-
-        RecommendationRule rule = new RecommendationRule();
+//
+//
+//        RecommendationRule rule = new RecommendationRule();
+//        UUID ruleId = rs.getObject("id", UUID.class);
+//
+//        rule.setId(ruleId);
+//        rule.setQuery(Queries.valueOf(rs.getString("query")));
+////        rule.setArguments((List<String>) rs.getArray("argument").getArray());
+//        rule.setNegate(rs.getBoolean("negate"));
+//
+//        String sql = "SELECT argument FROM arguments WHERE rule_id = ?";
+//        List<String> arguments = jdbcTemplate.queryForList(sql, String.class, ruleId);
+//        rule.setArguments(arguments);
+//
         UUID ruleId = rs.getObject("id", UUID.class);
 
-        rule.setId(ruleId);
-        rule.setQuery(Queries.valueOf(rs.getString("query")));
-//        rule.setArguments((List<String>) rs.getArray("argument").getArray());
-        rule.setNegate(rs.getBoolean("negate"));
+        RecommendationRule rule = ruleMap.get(ruleId);
+        if (rule == null) {
+            rule = new RecommendationRule();
+            rule.setId(ruleId);
+            rule.setQuery(Queries.valueOf(rs.getString("query")));
+            rule.setNegate(rs.getBoolean("negate"));
+            ruleMap.put(ruleId, rule);
+        }
 
-        String sql = "SELECT argument FROM arguments WHERE rule_id = ?";
-        List<String> arguments = jdbcTemplate.queryForList(sql, String.class, ruleId);
+        String argumentQuery = "SELECT argument FROM arguments WHERE rule_id = ?";
+        List<String> arguments = jdbcTemplate.queryForList(argumentQuery, String.class, ruleId);
         rule.setArguments(arguments);
 
         return rule;
+
+    }
+
+    public List<RecommendationRule> getRules() {
+        return new ArrayList<>(ruleMap.values());
     }
 }
