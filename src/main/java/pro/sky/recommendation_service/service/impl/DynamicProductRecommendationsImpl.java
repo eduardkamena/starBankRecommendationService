0a742804725598b17218d6c042a9efe87e653dc7
gitWestender <javaxwestender@gmail.com>
2024-10-31 21:57:55 +0300

    feature add getAvailableRecommendationsForUser by user UUID

--- src/main/java/pro/sky/recommendation_service/service/impl/DynamicProductRecommendationsImpl.java
+++ src/main/java/pro/sky/recommendation_service/service/impl/DynamicProductRecommendationsImpl.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Service;
 import pro.sky.recommendation_service.dto.DynamicRecommendationDTO;
 import pro.sky.recommendation_service.dto.RecommendationRuleDTO;
 import pro.sky.recommendation_service.entity.DynamicProductRecommendation;
+import pro.sky.recommendation_service.entity.RecommendationRule;
+import pro.sky.recommendation_service.entity.enums.Queries;
 import pro.sky.recommendation_service.repository.DynamicProductRecommendationsRepository;
 import pro.sky.recommendation_service.service.DynamicProductRecommendationsService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -32,4 +36,42 @@ public class DynamicProductRecommendationsImpl implements DynamicProductRecommen
         return dynamicProductRecommendationRepository.getAllRecommendations();
     }
 
+    public List<DynamicProductRecommendation> getAvailableRecommendationsForUser(UUID userId) {
+
+        //Получаем Map вида ID рекомендации, Список правил рекомендации
+        Map<UUID, List<RecommendationRule>> rulesMapByRecommendationId =
+                dynamicProductRecommendationRepository.getAllRecommendations().stream()
+                        .collect(Collectors.toMap(
+                                DynamicProductRecommendation::getId,
+                                RecommendationRule -> new ArrayList<>(RecommendationRule.getRecommendationRules()),
+                                (existingList, newList) -> {
+                                    existingList.addAll(newList);
+                                    return existingList;
+                                }
+                        ));
+
+        return getAllDynamicRecommendations().stream()
+                .filter(recommendation -> checkRules(userId, rulesMapByRecommendationId.get(recommendation.getId())))
+                .collect(Collectors.toList());
+    }
+
+    //Каждый IF выполняет подстановку в метод проверки аргументы из переданного правила
+    //Метод проверки возвращает Integer 1 или 0 и добавляет это значение к accessRules.
+    //Если rules.size() == выполненным проверкам - то добавляем продукт в return
+    public boolean checkRules(UUID Id, List<RecommendationRule> list) {
+        Integer accessRules = 0;
+        list.forEach(
+                rule -> {
+                    if (Queries.USER_OF.equals(rule.getQuery())) {
+                        return;
+                    } else if (Queries.ACTIVE_USER_OF.equals(rule.getQuery())) {
+                        return;
+                    } else if (Queries.TRANSACTION_SUM_COMPARE.equals(rule.getQuery())) {
+                        return;
+                    } else if (Queries.TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW.equals(rule.getQuery())) {
+                        return;
+                    }
+                });
+        return list.size() == accessRules;
+    }
 }
