3882e75132bbe7255c50e0c379d2b20bcd6b8a79
Ed <eduard@kamenskikh.me>
2024-11-08 02:32:49 +0300

    Добавил логирование статистики и кэширования

--- src/main/java/pro/sky/recommendation_service/controller/RuleStatsController.java
+++ src/main/java/pro/sky/recommendation_service/controller/RuleStatsController.java
@@ -2,6 +2,8 @@ package pro.sky.recommendation_service.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,8 @@ import java.util.stream.Collectors;
         description = "Выполняет действия со статистикой правил рекомендаций")
 public class RuleStatsController {
 
+    private final Logger logger = LoggerFactory.getLogger(RuleStatsController.class);
+
     private final RuleExecutionStatsService statsService;
 
     public RuleStatsController(RuleExecutionStatsService statsService) {
@@ -32,6 +36,8 @@ public class RuleStatsController {
             description = "Позволяет получить статистику срабатываний правил рекомендаций")
     public ResponseEntity<Map<String, List<Map<String, ? extends Number>>>> getRuleStats() {
 
+        logger.info("Receiving a request for counter response statistics");
+
         List<Map<String, ? extends Number>> statsList = statsService.getAllRuleExecutionStats().stream()
                 .map(stats -> {
                     Long ruleId = stats.getRuleId() != null ? stats.getRuleId() : 0L;
@@ -40,6 +46,8 @@ public class RuleStatsController {
                     return Map.of("rule_id", ruleId, "count", count);
                 })
                 .collect(Collectors.toList());
+
+        logger.info("Outputting in @Controller counter response statistics");
 
         return ResponseEntity.ok(Map.of("stats", statsList));
     }
--- src/main/java/pro/sky/recommendation_service/service/impl/RuleExecutionStatsServiceImpl.java
+++ src/main/java/pro/sky/recommendation_service/service/impl/RuleExecutionStatsServiceImpl.java
@@ -1,5 +1,7 @@
 package pro.sky.recommendation_service.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pro.sky.recommendation_service.entity.RuleExecutionStats;
 import pro.sky.recommendation_service.repository.RuleExecutionStatsRepository;
@@ -9,6 +11,9 @@ import java.util.List;
 
 @Service
 public class RuleExecutionStatsServiceImpl implements RuleExecutionStatsService {
+
+    private final Logger logger = LoggerFactory.getLogger(RuleExecutionStatsServiceImpl.class);
+
     private final RuleExecutionStatsRepository statsRepository;
 
     public RuleExecutionStatsServiceImpl(RuleExecutionStatsRepository statsRepository) {
@@ -17,19 +22,30 @@ public class RuleExecutionStatsServiceImpl implements RuleExecutionStatsService 
 
     @Override
     public void incrementRuleExecutionCount(Long ruleId) {
+
+        logger.info("Starting incrementing rule execution count for rule: {}", ruleId);
+
         RuleExecutionStats stats = statsRepository.findByRuleId(ruleId)
                 .orElse(new RuleExecutionStats(ruleId, 0));
         stats.setCount(stats.getCount() + 1);
         statsRepository.save(stats);
+
+        logger.info("Successfully added increment of rule execution count for rule: {}", ruleId);
     }
 
     @Override
     public void deleteRuleExecutionStats(Long ruleId) {
+
+        logger.info("Starting deleting rule execution count for rule: {}", ruleId);
         statsRepository.deleteById(ruleId);
+
+        logger.info("Successfully deleted rule execution count for rule: {}", ruleId);
     }
 
     @Override
     public List<RuleExecutionStats> getAllRuleExecutionStats() {
+
+        logger.info("Successfully got all rules execution stats");
         return statsRepository.findAll();
     }
 
